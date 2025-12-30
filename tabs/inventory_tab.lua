6f4c3e839b3529eecfc0fa64618372c6fc1edb75
doedie00-source <doeranong003@gmail.com>
2025-12-31 05:33:55 +0700

    Update inventory_tab.lua

--- tabs/inventory_tab.lua
+++ tabs/inventory_tab.lua
@@ -43,12 +43,12 @@ function InventoryTab:Init(parent)
     -- Header
     self.UIFactory.CreateLabel({
         Parent = parent,
-        Text = "HIDDEN TRADE",
+        Text = "  HIDDEN TRADE",
         Size = UDim2.new(1, -8, 0, 24),
         Position = UDim2.new(0, 8, 0, 0),
         TextColor = THEME.TextWhite,
         TextSize = 14,
-        Font = Enum.Font.GothamBold,
+        Font = Enum.Font.GothamBlack,
         TextXAlign = Enum.TextXAlignment.Left
     })
     
