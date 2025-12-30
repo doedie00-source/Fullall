6799cdc5e414ba61b6e463d58bccba751703e00f
doedie00-source <doeranong003@gmail.com>
2025-12-31 02:20:17 +0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -74,7 +74,8 @@ local THEME = {
     
     -- Accent Colors
     StarColor = Color3.fromRGB(255, 215, 0),
-    AccentBlue = Color3.fromRGB(0, 140, 255),       -- Primary Cyber Blue
+    AccentBlue = Color3.fromRGB(0, 140, 255),
+    AccentCyan = Color3.fromRGB(0, 200, 255),    
     AccentGreen = Color3.fromRGB(0, 220, 140),
 }
 
